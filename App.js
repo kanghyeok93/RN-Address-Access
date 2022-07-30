3831c4d385ab0bf7d3321c13deabbfd080800df3
smc p-n0055 <kanghyeok93@naver.com>
2022-07-30 19:52:43 +0900

    feat: Implementing the contacts feature

--- App.js
+++ App.js
@@ -1,9 +1,35 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-import {PermissionsAndroid} from 'react-native';
-import Contacts from 'react-native-contacts';
+import React, {useState} from 'react';
+import {PermissionsAndroid, Platform, Text, View} from 'react-native';
 
 const App = () => {
+  const [myContacts, setMyContacts] = useState([]);
+
+  const requestContactPermission = async () => {
+    if (Platform.OS === 'ios') {
+      console.warn('IOS');
+
+      return true;
+    } else {
+      console.warn('Android');
+
+      const granted = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.WRITE_CONTACTS,
+        PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+      ]);
+
+      if (
+        granted['android.permission.WRITE_CONTACTS'] ===
+          PermissionsAndroid.RESULTS.GRANTED &&
+        granted['android.permission.READ_CONTACTS'] ===
+          PermissionsAndroid.RESULTS.GRANTED
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
+
   return (
     <View>
       <Text>App</Text>
